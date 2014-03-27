d9158afef81611adbdc44aabae4fac50f0b4c669
Cesare Naldi <naldi.cesare@gmail.com>
2014-03-27 23:25:19 +0000

    Adds grunt-bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -212,6 +212,22 @@ module.exports = function (grunt) {
 				},
 				src: ['test/**/*.js']
 			}
+		},
+
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				updateConfigs: [],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json', 'dist/conduttore.js'], // '-a' for all files
+				createTag: true,
+				tagName: '%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: true,
+				pushTo: 'upstream',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
 		}
 	})
 
